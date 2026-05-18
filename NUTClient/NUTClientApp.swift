7ef1aa67293c72813f17b96cb67036ca3166193d
Barna Fulop <fbarni91@yahoo.com>
2026-05-18 20:35:15 +0300

    Initial Commit

---
+++ NUTClient/NUTClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  NUTClientApp.swift
+//  NUTClient
+//
+//  Created by Barna Fulop on 18.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct NUTClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
